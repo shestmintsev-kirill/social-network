8988a28a6185eeb42002828ad1ccd35119bd6c35
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-09-17 18:43:45 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/647px-Apple_logo_black.svg.png" alt="logo" />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav className="nav">
+        <div><a href="/">Profile</a></div>
+        <div><a href="/">Messages</a></div>
+        <div><a href="/">News</a></div>
+    </nav>
+  )
+}
+
+export default NavBar
