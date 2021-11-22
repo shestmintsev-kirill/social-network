e5ee7a9a9ea3f0dd24e192e924725328638bcd2b
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-11-22 12:55:42 +0300

    update

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -12,57 +12,58 @@ const Users = (props) => {
     pages.push(i);
   }
 
-  return (<div>
+  return (
     <div>
-      {pages.map(page => <span onClick={() => { props.onPageChanged(page) }} key={page} className={`${s.page} ${props.currentPage === page && s.activePage}`}>{page}</span>)}
-    </div>
-    {props.users.map(user => <div key={user.id}>
       <div>
-        <div className={s.avatarWrapper}>
-          <NavLink to={'/profile/' + user.id}>
-            <img src={user.photos.small ? user.photos.small : userPhoto} alt="user" />
-          </NavLink>
-        </div>
-        <div>
-          {user.followed
-            ? <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
-
-              props.toggleFollowingProgress(true, user.id);
-
-              usersAPI.getUnFollow(user.id).then((data) => {
-                if (data.resultCode === 0) {
-                  props.unFollow(user.id)
-                }
-                props.toggleFollowingProgress(false, user.id);
-              })
-
-            }}>Unfollow</button>
-            : <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
-
-              props.toggleFollowingProgress(true, user.id);
-
-              usersAPI.getFollow(user.id).then((data) => {
-                if (data.resultCode === 0) {
-                  props.follow(user.id)
-                }
-                props.toggleFollowingProgress(false, user.id);
-              })
-
-            }}>Follow</button>}
-        </div>
+        {pages.map(page => <span onClick={() => { props.onPageChanged(page) }} key={page} className={`${s.page} ${props.currentPage === page && s.activePage}`}>{page}</span>)}
       </div>
-      <div>
+      {props.users.map(user => <div key={user.id}>
         <div>
-          <div>{user.name}</div>
-          <div>{user.status}</div>
+          <div className={s.avatarWrapper}>
+            <NavLink to={'/profile/' + user.id}>
+              <img src={user.photos.small ? user.photos.small : userPhoto} alt="user" />
+            </NavLink>
+          </div>
+          <div>
+            {user.followed
+              ? <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
+
+                props.toggleFollowingProgress(true, user.id);
+
+                usersAPI.getUnFollow(user.id).then((data) => {
+                  if (data.resultCode === 0) {
+                    props.unFollow(user.id)
+                  }
+                  props.toggleFollowingProgress(false, user.id);
+                })
+
+              }}>Unfollow</button>
+              : <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
+
+                props.toggleFollowingProgress(true, user.id);
+
+                usersAPI.getFollow(user.id).then((data) => {
+                  if (data.resultCode === 0) {
+                    props.follow(user.id)
+                  }
+                  props.toggleFollowingProgress(false, user.id);
+                })
+
+              }}>Follow</button>}
+          </div>
         </div>
         <div>
-          <div>{'user.location.city'}</div>
-          <div>{'user.location.country'}</div>
+          <div>
+            <div>{user.name}</div>
+            <div>{user.status}</div>
+          </div>
+          <div>
+            <div>{'user.location.city'}</div>
+            <div>{'user.location.country'}</div>
+          </div>
         </div>
-      </div>
-    </div>)}
-  </div>)
+      </div>)}
+    </div>)
 }
 
 export default Users
