49fac3fb46c56d94827c6948042f0dd8b5b80347
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-11-30 11:03:02 +0300

    update

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Users from './Users'
 import Preloader from '../common/Preloader/Preloader';
 import { compose } from 'redux';
+import { getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount, getUsersState } from '../../redux/users-selectors';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
@@ -11,7 +12,8 @@ class UsersContainer extends React.Component {
   }
 
   onPageChanged = (page) => {
-    this.props.getUsers(page, this.props.pageSize)
+    this.props.getUsers(page, this.props.pageSize);
+    this.props.setCurrentPage(page);
   }
 
   render() {
@@ -36,12 +38,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress
+    users: getUsersState(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state)
   }
 }
 
