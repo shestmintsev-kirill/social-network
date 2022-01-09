19e003b03c0e64ba52305a1780b49982a1fe3521
shestmintsev-kirill <1kirillmihail1@gmail.com>
2022-01-09 21:29:14 +0300

    Updates

---
+++ static/js/7.6f2f0c30.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[7],{403:function(t,e,r){"use strict";r.r(e);var s=r(408),a=r(81),c=r(38),i=r(6);e.default=function(){return Object(i.jsx)(i.Fragment,{children:Object(i.jsx)(s.a,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:Object(i.jsx)(a.a,{type:"primary",children:Object(i.jsx)(c.b,{to:"/profile",children:"Back to home"})})})})}}}]);
+//# sourceMappingURL=7.6f2f0c30.chunk.js.map
