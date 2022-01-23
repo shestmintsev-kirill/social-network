65841d300fd709f1011de855750d1eef1628d28f
shestmintsev-kirill <1kirillmihail1@gmail.com>
2022-01-23 20:16:49 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import 'antd/dist/antd.css';
 import React, { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 import './App.css';
 import Preloader from './components/common/Preloader/Preloader';
 import Header from './components/Header/Header';
@@ -23,48 +24,60 @@ const ErrorPage = React.lazy(() => import('./pages/ErrorPage'));
 const App: React.FC = () => {
     const initialized = useSelector((state: AppStateType) => state.app.initialized);
     const dispatch = useDispatch();
+    const location = useLocation();
 
     useEffect(() => {
         dispatch(initializeApp());
     }, [dispatch]);
+
+    const transitions = useTransition(location, {
+        keys: (location) => location.pathname,
+        from: { display: 'none', transform: 'scale(0.9)', opacity: 0 },
+        enter: { display: 'block', transform: 'scale(1)', opacity: 1 },
+        leave: { display: 'none' }
+    });
 
     if (!initialized) {
         return <Preloader />;
     }
 
     return (
-        <Layout style={{ height: '100vh' }}>
+        <Layout style={{ height: '100vh', overflowX: 'hidden' }}>
             <Header />
             <Content style={{ height: '100%' }}>
                 <Layout className="site-layout-background" style={{ minHeight: '100%' }}>
                     <Navbar />
                     <Content style={{ padding: '0 24px', minHeight: '100%' }}>
                         <BreadCrumb />
-                        <Suspense fallback={<Skeleton active paragraph={{ rows: 6 }} />}>
-                            <Switch>
-                                <Route exact path="/">
-                                    <Redirect from="/" to="/profile" />
-                                </Route>
-                                <Route path="/dialogs">
-                                    <Dialogs />
-                                </Route>
-                                <Route path="/profile/:userId?">
-                                    <Profile />
-                                </Route>
-                                <Route path="/developers">
-                                    <Users />
-                                </Route>
-                                <Route path="/login">
-                                    <Login />
-                                </Route>
-                                <Route path="/chat">
-                                    <ChatPage />
-                                </Route>
-                                <Route path="*">
-                                    <ErrorPage />
-                                </Route>
-                            </Switch>
-                        </Suspense>
+                        {transitions((props, item) => (
+                            <animated.div style={props}>
+                                <Suspense fallback={<Skeleton active paragraph={{ rows: 6 }} />}>
+                                    <Switch location={item}>
+                                        <Route exact path="/">
+                                            <Redirect from="/" to="/profile" />
+                                        </Route>
+                                        <Route path="/dialogs">
+                                            <Dialogs />
+                                        </Route>
+                                        <Route path="/profile/:userId?">
+                                            <Profile />
+                                        </Route>
+                                        <Route path="/developers">
+                                            <Users />
+                                        </Route>
+                                        <Route path="/login">
+                                            <Login />
+                                        </Route>
+                                        <Route path="/chat">
+                                            <ChatPage />
+                                        </Route>
+                                        <Route path="*">
+                                            <ErrorPage />
+                                        </Route>
+                                    </Switch>
+                                </Suspense>
+                            </animated.div>
+                        ))}
                         <Footer style={{ textAlign: 'center' }}>Sosial network 2022</Footer>
                     </Content>
                 </Layout>
